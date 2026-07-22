1c5236f536b7f204d60701f647af37e0f69736ea
Your Name <maheshwariaadhya77@gmail.com>
2026-07-22 11:58:51 +0530

    Remove duplicate gmail stats route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import gmailRouter from './routes/gmail.js';
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
